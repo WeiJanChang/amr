9ddd1c7d86ecc43dd964436639e888f3b5207730
Wei-Jan Chang <cc9868422@gmail.com>
2023-07-27 00:45:52 +0100

    wrote down notes on Topic Modeling and got probabilities on 11 Hashtags

--- amr/src/test.py
+++ amr/src/test.py
@@ -36,10 +36,8 @@ import demoji
 import webbrowser
 import string
 from itertools import chain  # To merge multiple lists into a single list
-import nltk  # Natural Language Toolkit
-from nltk.corpus import words
-import enchant
 from langdetect import detect, LangDetectException
+import warnings
 
 
 def remove_emojis(text):
@@ -169,7 +167,7 @@ def organised_data(df: pd.DataFrame,
 
 
 if __name__ == '__main__':
-    df = load_json('/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship')
+    df = load_json('/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antibiotics')
     # Print the captions and URLs for easy reference
     df['Caption'], df['URL'] = zip(*df['latestPosts'].apply(extract_captions_urls))
 
@@ -321,39 +319,34 @@ new_df['name'] = new_df['name'].mask(new_df['name'].duplicated(), '')
 new_df['url'] = new_df['url'].mask(new_df['url'].duplicated(), '')
 
 # new_df.to_csv(
-#     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship/Antimicrobial stewardship 01 Jan 2017 - 01 July 2023_specific hashtags.csv')
+#     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship/Antimicrobial stewardship 01 Jan 2017 - 01 July 2023_specific hashtags.csv',
+#     index=False)
 
 print("Data successfully processed and saved to modified csv file.")
 
 """"Topic modelling"""
-
 import gensim  # the library for Topic modelling
 from gensim.models.ldamulticore import LdaMulticore
 from gensim import corpora, models
 import pyLDAvis.gensim  # LDA visualization library
-from lda import lda
-from IPython.display import HTML
 from nltk.corpus import stopwords, words
 from nltk.stem.wordnet import WordNetLemmatizer
 
-# Step 1: clean the data
+"""Step 1: clean the data"""
 
-stop = set(stopwords.words('english'))  # Examples of stopwords include "the," "a," "an," "in," "on," etc. The
-# stopwords module from the nltk library provides a list of common stopwords in different languages, and here we are
-# using the ones for English
-
+stop = set(stopwords.words('english'))
+"""Examples of stopwords include "the," "a," "an," "in," "on," etc. The stopwords module from the nltk library 
+provides a list of common stopwords in different languages, and here we are using the ones for English"""
 exclude = set(string.punctuation)
-lemma = WordNetLemmatizer()  # For example, the lemma of the words "running," "runs," and "ran" is "run." The
-
-
-# WordNetLemmatizer class uses the WordNet lexical database to perform lemmatization. This helps reduce inflected
-# words to a common base form, which can be useful for text analysis and processing tasks
+lemma = WordNetLemmatizer()
+"""For example, the lemma of the words "running," "runs," and "ran" is "run." The WordNetLemmatizer class uses the 
+WordNet lexical database to perform lemmatization. This helps reduce inflected words to a common base form, 
+which can be useful for text analysis and processing tasks"""
 
 
 def clean(text):
     # remove hashtags
     text_without_hashtags = ' '.join([word for word in text.lower().split() if not word.startswith('#')])
-
     english_vocab = set(words.words())
     words_only_english = [word for word in text.split() if word.lower() in english_vocab]
 
@@ -361,70 +354,97 @@ def clean(text):
     stop_free = [word for word in text_without_hashtags.split() if word not in stop]
     punc_free = [ch for ch in stop_free if ch not in exclude]
     normalized = [lemma.lemmatize(word) for word in punc_free]
+
     # Combine normalized with words_only_english
     normalized.extend(words_only_english)
-    return normalized
+
+    return normalized  # no stopwords, no punc, no hashtags
 
 
-new_df['Caption_clean'] = new_df['Caption'].apply(clean)
+new_df['Caption_cleaned'] = new_df['Caption'].apply(clean)
 
-# Step 2: Create Dictionary from the articles
-dictionary = corpora.Dictionary(new_df['Caption_clean'])
+"""Step 2: Create a dictionary from new_df['Caption_cleaned']"""
+dictionary = corpora.Dictionary(new_df['Caption_cleaned'])
+# print(dictionary) --> 1925 unique words
 
-# Step 3: Create document term matric
-doc_term_matrix = [dictionary.doc2bow(doc) for doc in new_df['Caption_clean']]  # The doc2bow function from the
-# dictionary object is used to convert each preprocessed document (in the form of a list of words) into a
-# bag-of-words representation. It returns a list of tuples, where each tuple contains the word's ID and its frequency
-# in the document. The resulting doc_term_matrix is a list of such tuples for each document.
+"""Step 3: Create document term matrix"""
+doc_term_matrix = [dictionary.doc2bow(doc) for doc in new_df['Caption_cleaned']]
+# print(doc_term_matrix) --> calculate a word  show how many times
+# The doc2bow function: Convert each text in new_df['Caption_clean'] to document-term representation.
+# print(dictionary.num_nnz) --> non-repeated words
+# print(len(doc_term_matrix)) --> a total words
 
-# print(dictionary.num_nnz)
-# print(len(doc_term_matrix))
-
-# Step 4: Instantiate LDA model
+"""Step 4: Instantiate LDA model"""
 lda = gensim.models.ldamodel.LdaModel
+"""This algorithm assumes that each document in the text is composed of different proportions of topics, 
+and each topic is composed of different proportions of words. LDA finds these latent topics and their word 
+combinations through an iterative process"""
 
-# Step 5: print the topics identified by LDA model
+"""Step 5: print the topics identified by LDA model"""
 # can't overlapping the circle (see on the web)--> If overlapped--> not a good model fit --> shorter the num_topics
-num_topics = 3
-ldamodel = lda(doc_term_matrix, num_topics=num_topics, id2word=dictionary, passes=50, minimum_probability=0)
-# doc_term_matrix: The document-term matrix created in Step 3.
-# num_topics: The number of topics to be identified by the LDA model (in this case, set to 3).
-# id2word: The dictionary created in Step 2, which maps word IDs to words.
-# passes: The number of passes through the entire corpus during training.
-# minimum_probability: The minimum probability value required for a word to be considered in a topic
-# (set to 0, meaning all words are included).
+num_topics = 3  # num_topics: The number of topics to be identified by the LDA model
+ldamodel = lda(doc_term_matrix, num_topics=num_topics, id2word=dictionary, passes=50, minimum_probability=0.1,
+               random_state=50)
+"""id2word: The dictionary created in Step 2, which maps word IDs to words. 
+passes: the number of times the algorithm goes through all the documents in the dataset during the training process. 
+Each pass allows the model to learn and update its understanding of the data, potentially improving the quality of the 
+identified topics.
+minimum_probability:  The minimum probability value required for a word to be considered in a topic. 
+In this case, it's set to 0, meaning all words will be included in the topics regardless of 
+their probability. If set to a higher value (e.g., 0.01), the model will only include words with a probability 
+greater than or equal to the specified value.
+random_state: """
 print(ldamodel.print_topics(num_topics=num_topics))
 
-# Step 6:Visualize the LDA model results
-lda_display = pyLDAvis.gensim.prepare(ldamodel, doc_term_matrix, dictionary, sort_topics=False, mds='mmds')
-# save to HTML that can open on web
-pyLDAvis.save_html(lda_display, 'LDA_Visualization.html')
+"""Step 6:Visualize the LDA model results"""
+# warnings.simplefilter(action='ignore', category=FutureWarning)
+# lda_display = pyLDAvis.gensim.prepare(ldamodel, doc_term_matrix, dictionary, sort_topics=False, mds='mmds')
+# # save to HTML that can open on web
+# pyLDAvis.save_html(lda_display, 'LDA_Visualization.html')
+# webbrowser.open('file://' + os.path.realpath('LDA_Visualization.html'))
 
-webbrowser.open('file://' + os.path.realpath('LDA_Visualization.html'))
-
-# Step 7: Find which articles were marked in which cluster
+"""Step 7: Find which articles were marked in which cluster"""
 # Assigns the topics to the documents in corpus
-lda_corpus = ldamodel[doc_term_matrix]
-print([doc for doc in lda_corpus])
-scores = list(chain(*[[score for topic_id, score in topic] \
-                      for topic in [doc for doc in lda_corpus]]))
+topic_distribution = ldamodel[doc_term_matrix]  # contains the topic distribution for each document.
+# print([doc for doc in topic_distribution]) --> This result shows the topic distribution for each document. Each
+# document is represented by a list, where each element in the list represents a topic along with its corresponding
+# probability.
+scores = list(chain(*[[score for topic_id, score in topic] for topic in [doc for doc in topic_distribution]]))
+# This line extracts the probability scores for all topics for each document and stores them in the scores list
 threshold = sum(scores) / len(scores)
+"""The threshold is calculated as the average of all probability scores. It's used as a threshold to determine which 
+articles belong to which cluster. Articles with a probability score greater than this threshold are considered to 
+belong to a cluster"""
 print(threshold)
+"""After computing the threshold, you can use it to filter out topics that have probability scores below this 
+threshold. Topics with probabilities lower than the threshold are considered less significant or relevant, 
+and you may choose to exclude them from further analysis or visualization. This threshold can help you focus on the 
+most important and representative topics in your topic modeling results."""
 
-cluster1 = [j for i, j in zip(lda_corpus, df.index) if i[0][1] > threshold]
-cluster2 = [j for i, j in zip(lda_corpus, df.index) if i[0][1] > threshold]
-cluster3 = [j for i, j in zip(lda_corpus, df.index) if i[0][1] > threshold]
-print(len(cluster1))
-print(len(cluster2))
-print(len(cluster3))
+"""
+Each Threshold
+1. AMR: 0.8284
+2. Antimicrobial resistance: 0.7608
+3. Antibiotics: 0.7205
+4. Antimicrobials: 0.7420
+5. Antimicrobial stewardship: 0.8535
+6. Drug resistant: 0.8302
+7. Superbugs: 0.8308
+8. Antibiotic resistance: 0.7203
+9. Infections: 0.6630
+10. Bacterial infections:  0.9411
+11. Antibiotic prescribing: 0.7825
+"""
 
-print(new_df.iloc[cluster1])
-print(new_df.iloc[cluster2])
-clusterdf = new_df.iloc[cluster3]
-clusterdf.to_csv(
-    '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antibiotics/Antibiotics 01 Jan 2017 - 01 July 2023_topic texts cluster test.csv',
-    index=False)
-
-new_df.to_csv(
-    '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antibiotics/Antibiotics 01 Jan 2017 - 01 July 2023_topic texts test.csv',
-    index=False)
+# cluster1 = [j for i, j in zip(lda_corpus, new_df.index) if i[0][1] > threshold]
+# cluster2 = [j for i, j in zip(lda_corpus, new_df.index) if i[1][1] > threshold]
+# cluster3 = [j for i, j in zip(lda_corpus, new_df.index) if i[2][1] > threshold]
+#
+# print(new_df.iloc[cluster1])
+# clusterdf = new_df.iloc[cluster3]
+# new_df.to_csv(
+#     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship/Antimicrobial stewardship 01 Jan 2017 - 01 July 2023_ selected words.csv',
+#     index=False)
+# clusterdf.to_csv(
+#     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship/Antimicrobial stewardship 01 Jan 2017 - 01 July 2023_topic texts cluster test.csv',
+#     index=False)
