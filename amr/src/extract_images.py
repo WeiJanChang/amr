fbaecaa5392adaee581a9bd79e6e74c2f7bb10dd
Wei-Jan Chang <cc9868422@gmail.com>
2023-08-02 17:31:08 +0100

    download images but incomplete

--- amr/src/extract_images.py
+++ amr/src/extract_images.py
@@ -4,10 +4,56 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from PIL import Image
 import instaloader
+import requests
 
 all_ig = pd.read_csv(
     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded).csv')
 
+
+# can't download complete images
+def download_instagram_image(post_url, name, ID, save_path):
+    try:
+        response = requests.get(post_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # search URL
+        image_url = soup.find('meta', property='og:image')['content']
+
+        # download images
+        image_response = requests.get(image_url)
+        image_response.raise_for_status()
+        with open(save_path, 'wb') as f:
+            f.write(image_response.content)
+
+        print(f"Images {name}_{ID} saved as {save_path}")
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else", err)
+    except (IOError, ValueError, OSError) as err:
+        print("images error:", err)
+    except TypeError:
+        print("can't find correct URL")
+
+
+# save path
+output_folder = 'Instagram images'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for index, row in all_ig.iterrows():
+    instagram_url = row['URL']
+    image_name = f"{row['name']}_{row['ID']}.jpg"
+    image_path = os.path.join(output_folder, image_name)
+    download_instagram_image(instagram_url, row['name'], row['ID'], image_path)
+
+"""
+#NEED API
 def download_instagram_image(post_url, name, ID, save_path):
     try:
         loader = instaloader.Instaloader()
@@ -39,4 +85,4 @@ for index, row in all_ig.iterrows():
     image_path = output_folder
     download_instagram_image(instagram_url, row['name'], row['ID'], image_path)
 
-
+"""
