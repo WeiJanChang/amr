183fa06df261ee98510e30824ca8cc5936baf472
Wei-Jan Chang <cc9868422@gmail.com>
2023-08-28 02:08:51 +0200

    get latest post

--- amr/src/igjson.py
+++ amr/src/igjson.py
@@ -3,15 +3,13 @@ from pathlib import Path
 from typing import TypedDict, Any
 
 
-
-
-class PostsDict(TypedDict):
+class PostsDict(TypedDict):  # topPosts
     id: str
     type: str
     shortCode: str
     caption: str
     hashtags: list[str]
-    mentions: Any  # TODO
+    mentions: list[str]
     url: str
     commentsCount: int
     firstComment: str
@@ -23,7 +21,7 @@ class PostsDict(TypedDict):
     alt: Any | None
     likesCount: int
     timestamp: str
-    childPosts: list[...]
+    childPosts: list
     ownerId: str
 
 
@@ -34,40 +32,50 @@ class DownLoadDict(TypedDict):
     topPostsOnly: bool
     profilePicUrl: str
     postsCount: int
-    topPosts: list[PostsDict | None]
+    topPosts: list[PostsDict | None]  # Either PostsDict or None
     latestPosts: list[PostsDict | None]
 
-DOWNLOAD_JSON = list[DownLoadDict]
+
+DOWNLOAD_JSON = list[DownLoadDict]  # list of DownLoadDict
+
 
 def load_json(p) -> DOWNLOAD_JSON:
     import json
-    with open(p, 'rb') as f:
+    with open(p, 'rb') as f:  # rb: read non-text
         ret = json.load(f)
 
     return ret
 
+
 def get_lastest_post(j: DOWNLOAD_JSON) -> list[PostsDict]:
-    pass
+    """
+    from json file get "latestPosts" from different posts
+
+    :param j:
+    :return:
+    """
+    for post in j:  # type: PostsDict
+
+        print(post["latestPosts"])
 
 
 def load(file: Path):
-    # TODO pickle
+    # TODO pickle # 有pickle 就叫出 沒有就
     if file.exists():
         with open(file, 'rb') as f:
             return pickle.load(file)
     else:
         save(file)
 
+
 def save(output: Path):
     pass
 
 
-
-
-
-
 if __name__ == '__main__':
-    p = '/Users/wei/Python/caranetwork/amr/test_file/test.json'
-    j= load_json(p)
-    ret = foreach_get(j, 'latestPosts', 'id')
-    print(ret)
+    p = '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/Antimicrobial stewardship/Antimicrobial stewardship 01 Jan 2017 - 01 July 2023.json'
+    j = load_json(p)
+    # ret = foreach_get(j, 'latestPosts', 'id')
+    # print(ret)
+    print(type(j[0]))
+    print(get_lastest_post(j))
