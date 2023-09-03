53a4447f5b8db15921dd52bb84163b73f0d01981
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-03 17:05:02 +0200

    type hint

--- amr/src/extract_images.py
+++ amr/src/extract_images.py
@@ -7,14 +7,13 @@ import cv2
 import numpy as np
 import pytesseract
 
-
 instagram_df = pd.read_csv(
     '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded).csv')
 
 
 # Step 1 Download images from all_Instagram_data(non-English excluded).csv
 
-def download_instagram_image(post_url, name, ID, save_path):
+def download_instagram_image(post_url: str, name: str, ID: int, save_path: list[str]):
     try:
         loader = instaloader.Instaloader()
 
@@ -42,6 +41,7 @@ def download_instagram_image(post_url, name, ID, save_path):
 
 def main_instagram():
     # save path
+    # todo: os -> path
     output_folder = 'Instagram images'
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -51,7 +51,6 @@ def main_instagram():
         image_name = f"{row['name']}_{row['ID']}.jpg"
         image_path = output_folder
         download_instagram_image(instagram_url, row['name'], row['ID'], image_path)
-
 
 
 def extract_text(file: Union[Path, str],
@@ -79,7 +78,6 @@ def extract_text(file: Union[Path, str],
         plt.title(extract)
         plt.show()
 
-
     return has_text, extract
 
 
@@ -94,8 +92,8 @@ def _preprocess_image(image: np.ndarray) -> np.ndarray:
 
 
 if __name__ == '__main__':
-# run .jpg in files and return img
-# todo: how to save them into csv and don't return each img. need run them all
+    # run .jpg in files and return img
+    # todo: how to save them into csv and don't return each img. need run them all
 
     directory = '/Users/wei/Library/CloudStorage/GoogleDrive-wei-jan.chang@ucdconnect.ie/.shortcut-targets-by-id/10hvZ9DULDsQxq93eg2SZ3tFyFCP37DnA/Wei-Jan/Image,Video with texts_Instagram 2/selected'
     jpg_files = list(Path(directory).glob('*.jpg'))
@@ -104,6 +102,3 @@ if __name__ == '__main__':
     for file_path in jpg_files:
         has_text, extract = extract_text(file_path, config="--psm 6")
         results.append({'Image_Path': str(file_path), 'Extracted_Text': extract})
-
-    df = pd.DataFrame(results)
-    df.to_csv('/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/image_extracted_text.csv', index=False)
