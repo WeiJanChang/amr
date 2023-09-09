efe2fdec149b3a65a7d10db21908b75a280735db
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-09 17:34:48 +0200

    modified. todo

--- amr/src/extract_images.py
+++ amr/src/extract_images.py
@@ -1,3 +1,11 @@
+"""
+pipeline
+
+Step 1: Download images from all_Instagram_data(non-English excluded).csv
+
+Step 2: extract texts from images
+
+"""
 import os
 import pandas as pd
 import instaloader
@@ -8,25 +16,28 @@ import numpy as np
 import pytesseract
 
 instagram_df = pd.read_csv(
-    '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded).csv')
+    '/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded).csv')
 
 
-# Step 1 Download images from all_Instagram_data(non-English excluded).csv
-
-def download_instagram_image(post_url: str, name: str, ID: int, save_path: list[str]):
+# Step 1
+def download_instagram_image(post_url: str,
+                             name: str,
+                             id: str,
+                             save_path: list[str]) -> bool:
     try:
         loader = instaloader.Instaloader()
 
         post = instaloader.Post.from_shortcode(loader.context, post_url.split('/')[-2])
 
         # Generate the filename using the provided name and ID
-        image_name = f"{name}_{ID}.jpg"
+        image_name = f"{name}_{id}.jpg"
 
         # Combine the save_path and image_name to get the full file path
         full_file_path = os.path.join(save_path, image_name)
 
         loader.download_post(post, target=full_file_path)
-        print(f"Image {name}_{ID} saved as {full_file_path}")
+        print(f"Image {name}_{id} saved as {full_file_path}")
+        return True
     except instaloader.exceptions.InvalidArgumentException:
         print("can't open URL")
     except instaloader.exceptions.ConnectionException as e:
@@ -38,19 +49,7 @@ def download_instagram_image(post_url: str, name: str, ID: int, save_path: list[
     except Exception as e:
         print("download error:", e)
 
-
-def main_instagram():
-    # save path
-    # todo: os -> path
-    output_folder = 'Instagram images'
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    for index, row in instagram_df.iterrows():
-        instagram_url = row['URL']
-        image_name = f"{row['name']}_{row['ID']}.jpg"
-        image_path = output_folder
-        download_instagram_image(instagram_url, row['name'], row['ID'], image_path)
+    return False
 
 
 def extract_text(file: Union[Path, str],
@@ -93,12 +92,22 @@ def _preprocess_image(image: np.ndarray) -> np.ndarray:
 
 if __name__ == '__main__':
     # run .jpg in files and return img
-    # todo: how to save them into csv and don't return each img. need run them all
+    # todo: how to save them into csv and don't return each img. need run them all; create col for images, another col for extract txt
 
-    directory = '/Users/wei/Library/CloudStorage/GoogleDrive-wei-jan.chang@ucdconnect.ie/.shortcut-targets-by-id/10hvZ9DULDsQxq93eg2SZ3tFyFCP37DnA/Wei-Jan/Image,Video with texts_Instagram 2/selected'
+    directory = '/Users/wei/Library/CloudStorage/GoogleDrive-wei-jan.chang@ucdconnect.ie/.shortcut-targets-by-id/10hvZ9DULDsQxq93eg2SZ3tFyFCP37DnA/Wei-Jan/Instagram 1+2'
     jpg_files = list(Path(directory).glob('*.jpg'))
 
     results = []
     for file_path in jpg_files:
-        has_text, extract = extract_text(file_path, config="--psm 6")
+        has_text, extract = extract_text(file_path, config="r'--psm 6'")
         results.append({'Image_Path': str(file_path), 'Extracted_Text': extract})
+
+    # output_folder = '/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/Instagram images'
+    # if not os.path.exists(output_folder):
+    #     os.makedirs(output_folder)
+    # download images
+    # for index, row in instagram_df.iterrows():
+    #     instagram_url = row['URL']
+    #     image_name = f"{row['name']}_{row['ID']}.jpg"
+    #     image_path = output_folder
+    #     download_instagram_image(instagram_url, row['name'], row['ID'], image_path)
