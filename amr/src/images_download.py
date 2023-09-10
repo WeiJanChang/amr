c6f3fe06bcd92fb17a655dba913c2d04dcb147e2
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-10 11:00:34 +0200

    extract url from json

--- amr/src/images_download.py
+++ amr/src/images_download.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Union
 
+import instaloader
+
 from amr.src.Ig_info_practice import load_from_directory, LatestPostInfo
 
 PathLike = Union[Path | str]
@@ -13,19 +15,24 @@ def create_latestpost_info(directory: PathLike) -> LatestPostInfo:
     return ret.remove_unused_fields().remove_duplicate()
 
 
-def get_url_from_latestpost(info: LatestPostInfo) -> list[str]:
-    df = info.to_dataframe()
-    url = list(df['url'])
-    return url
+def download_image(info: LatestPostInfo) -> bool:
+    df = info.to_dataframe().to_pandas()  # polar df to pandas df
+    post_url = df['url'].to_list()
+    return post_url
 
+    loader = instaloader.Instaloader()
+    # post = instaloader.Post.from_shortcode(loader.context, post_url.split('/')[-2])
 
-def download_image():
-    pass
+    image_name = f"{id}.jpg"
+    # file_path = ''
+    # loader.download_post(post, target=file_path)
+
+    return post_url
 
 
 if __name__ == '__main__':
     d = '/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/json file'
     info = create_latestpost_info(d)
-    urls = get_url_from_latestpost(info=info)
+    urls = download_image(info)
     print(urls)
-
+    print('https://www.instagram.com/p/CuroezmPzbb/'.split('/')[-2])
