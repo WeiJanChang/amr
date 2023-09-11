2d1bd44fc45cece7f3797d2e32ca839939d07687
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-11 16:43:31 +0200

    find unique id, not caption

--- amr/src/merge_file.py
+++ amr/src/merge_file.py
@@ -2,29 +2,27 @@
 pipeline
 
 1. merge each csv file (11 keywords)
-2. find duplicate Caption
-3. Mark duplicate Caption
-4. Remove duplicate Caption and save to a new csv file
+2. find duplicate ID
+3. Remove duplicate ID and save to a new csv file
 """
 import pandas as pd
-from typing import Union
+from typing import Union, Optional
 from pathlib import Path
 
-def mark_duplicates_data(file: Union[Path, str],
-                         output_file_path):
+def find_duplicated(file: Path | str,
+                    save_path: Optional[Path] = None) -> bool:
     """
     read csv file, find duplicate row in Caption and add ****** in Index, save as new csv file
-
     """
     try:
-        df = pd.read_csv(str(file))
+        df = pd.read_csv(file)
 
         # find duplicate row
-        duplicates = df[df.duplicated(subset='Caption', keep=False)]
+        duplicates = df[df.duplicated(subset='ID', keep=False)]
         duplicate_captions_index = duplicates.index
 
         # Group duplicate rows by 'Caption' and get the corresponding indices
-        duplicate_indices = duplicates.groupby('Caption').apply(lambda x: x.index.tolist())
+        duplicate_indices = duplicates.groupby('ID').apply(lambda x: x.index.tolist())
 
         # Output duplicate indices
         print("Duplicate Indices:")
@@ -38,9 +36,9 @@ def mark_duplicates_data(file: Union[Path, str],
 
         # save
         duplicate_data.to_csv(output_file_path)
-        duplicate_data.drop_duplicates(subset='Caption', keep='first', inplace=True)
+        duplicate_data.drop_duplicates(subset='ID', keep='first', inplace=True)
         duplicate_data.to_csv(
-            '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded)- remove duplicate marked.csv')
+            '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded)- remove duplicate id marked.csv')
         print("duplicate index：", duplicate_captions_index.tolist())
         print('row (After removing):', duplicate_data.shape[0])
         print('duplicate rows in total:', 3355 - duplicate_data.shape[0])
@@ -85,5 +83,5 @@ if __name__ == '__main__':
 
 
     p = '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded).csv'
-    save_path = '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded)- duplicate marked.csv'
-    mark_duplicates_data(p, save_path)
+    save_path = '/Users/wei/Job Application 2023/CARA Network/AMR /AMR Instagram data/all_Instagram_data(non-English excluded)- duplicate id marked.csv'
+
