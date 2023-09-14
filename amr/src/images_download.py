34d74914f73ebae34126e15efe09ba934d9fc291
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-14 21:43:42 +0200

    number of videos/images to df

--- amr/src/images_download.py
+++ amr/src/images_download.py
@@ -1,9 +1,13 @@
-import shutil
+import re
+
+import numpy as np
+import pandas as pd
+import polars as pl
 import time
 from pathlib import Path
-from typing import Union, Tuple, List, Any
+from typing import Union
 import instaloader
-from instaloader import InstaloaderContext, Instaloader, InstaloaderException
+from instaloader import InstaloaderContext, Instaloader, InstaloaderException, ConnectionException
 from amr.src.Ig_info import load_from_directory, LatestPostInfo
 
 # todo: download images and video only with all enlgish Caption and hashtags,
@@ -60,22 +64,31 @@ def _download(loader: Instaloader, context: InstaloaderContext, post_id: str, po
 
 
 def download_postprocess(output_path: PathLike, new_dir: PathLike, verbose: bool = True):
-    # todo: create a df, col = id, number of images, and number of mp4, number of txt
-    #  join this df to original_df based on id
+    id_list = []
+    num_jpg_list = []
+    num_video_list = []
     for path in Path(output_path).iterdir():  # select all folders in output_path
         if path.is_dir():  # whether path is directory
             jpg_files = sorted(path.glob("*.jpg"))  # Sort the files for consistent numbering
             video_files = sorted(path.glob("*.mp4"))
             txt_files = sorted(path.glob("*.txt"))
+
             for i, jpg_file in enumerate(jpg_files, start=1):
                 new_jpg_file = new_dir / f"{path.name}_{i}{jpg_file.suffix}"
                 jpg_file.rename(new_jpg_file)
+                id = re.split(r'(\d+)', (str(new_jpg_file).split('/')[-1]), 1)[1]  # type: str # \d+ integer part
+                id_list.append(id)
+                num_jpg_list.append(len(jpg_files))
                 if verbose:
                     print(f"{jpg_file} -> {new_jpg_file}")
 
             for j, video_file in enumerate(video_files, start=1):
                 new_video_file = new_dir / f"{path.name}_{j}{video_file.suffix}"
-                video_file.rename(video_file)
+                video_file.rename(new_video_file)
+                id = re.split(r'(\d+)', (str(new_video_file).split('/')[-1]), 1)[1]
+                id_list.append(id)
+                num_video_list.append(len(video_files))
+
                 if verbose:
                     print(f"{video_file} -> {new_video_file}")
 
@@ -85,11 +98,23 @@ def download_postprocess(output_path: PathLike, new_dir: PathLike, verbose: bool
                 if verbose:
                     print(f"{txt_file} -> {new_txt_file}")
 
+    if len(num_video_list) == 0 in num_video_list:
+        num_video_list.append('0')
+        jpg_df = pd.DataFrame({'id': id_list, 'number of jpg': num_jpg_list})
+        video_df = pd.DataFrame({'id': id_list, 'number of video': num_video_list})
+        final_df = jpg_df.merge(video_df, on='id', how='outer')
+    else:
+        jpg_df = pd.DataFrame({'id': id_list, 'number of jpg': num_jpg_list})
+        final_df = jpg_df.copy()
+        final_df['number of video'] = '0'
+    return final_df
+
 
 if __name__ == '__main__':
-    d = '/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/json file'
+    d = '/Users/wei/Documents/CARA Network/AMR/AMR Instagram data/json file'
     info = create_latestpost_info(d)
-    output_path = Path('/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/Instagram images')
+    output_path = Path('/Users/wei/Documents/CARA Network/AMR/AMR Instagram data/instagram_images_with_dir')
     download_image(info, output_path)
-    new_dir = Path('/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/images')
-    download_postprocess(output_path, new_dir)
+    new_dir = Path('/Users/wei/Documents/CARA Network/AMR/AMR Instagram data/all_instagram_images')
+    df = download_postprocess(output_path, new_dir)
+    df.to_excel('/Users/wei/Documents/CARA Network/AMR /AMR Instagram data/number_of_images_and_videos_with_id.xlsx')
