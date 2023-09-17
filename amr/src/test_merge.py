fa6fdb8fa4214ce965c052cbc97dc74004c53113
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-18 01:38:32 +0200

    merge df1,2,3,4

--- amr/src/test_merge.py
+++ amr/src/test_merge.py
@@ -6,8 +6,7 @@ error_df2 = pl.read_csv('/Users/wei/Documents/cara_network/amr_igdata/error_out_
 post_pro_df3 = pl.read_csv('/Users/wei/Documents/cara_network/amr_igdata/output/n_images_video_with_id.csv')
 merge_df = post_pro_df3.join(error_df2, on='id', how='outer')
 merge_df = original_df.join(merge_df, on='id')
-merge_df = original_df.join(post_pro_df3, on='id')
-# merge_df.write_csv('/Users/wei/Documents/cara_network/amr_igdata/merge_test.csv')
+merge_df.write_csv('/Users/wei/Documents/cara_network/amr_igdata/merge_test.csv')
 
 ret = collections.defaultdict(list)
 label_df = pl.read_csv('/Users/wei/code/rscvp/src/rscvp/util/image/test2.csv')
@@ -24,6 +23,6 @@ df = pl.DataFrame(ret)
 label_df = df.pivot(index='id', columns='id_', values='notes')
 label_df = label_df.with_columns(pl.col('id').cast(pl.Int64))
 
-final_df = merge_df.join(label_df, on='id')
+final_df = label_df.join(merge_df, on='id',how='outer')
 
 final_df.write_excel('/Users/wei/Documents/cara_network/amr_igdata/final_dftest.xlsx')
