ae2bea47b2cb6296996fef4e17daff41246915b2
Wei-Jan Chang <cc9868422@gmail.com>
2023-09-18 17:13:25 +0200

    merge all df in function

--- amr/src/test_merge.py
+++ amr/src/test_merge.py
@@ -1,28 +1,49 @@
 import collections
+from pathlib import Path
+from typing import Union
+
 import polars as pl
 
-original_df = pl.read_excel('/Users/wei/Documents/cara_network/amr_igdata/output/original_instagram_data.xlsx')
-error_df2 = pl.read_csv('/Users/wei/Documents/cara_network/amr_igdata/error_out_test.csv')
-post_pro_df3 = pl.read_csv('/Users/wei/Documents/cara_network/amr_igdata/output/n_images_video_with_id.csv')
-merge_df = post_pro_df3.join(error_df2, on='id', how='outer')
-merge_df = original_df.join(merge_df, on='id')
-merge_df.write_csv('/Users/wei/Documents/cara_network/amr_igdata/merge_test.csv')
+PathLike = Union[Path | str]
 
-ret = collections.defaultdict(list)
-label_df = pl.read_csv('/Users/wei/code/rscvp/src/rscvp/util/image/test2.csv')
-id = label_df['filename']
-note = label_df['notes']
-for ids in id:
-    ret['id'].append(ids.split('_')[1])
-    ret['id_'].append(ids.split('_')[2])
-for notes in note:
-    ret['notes'].append(notes)
 
-df = pl.DataFrame(ret)
+def merge_all(dir_path: PathLike, final_out: PathLike = None) -> pl.DataFrame:
+    ret = collections.defaultdict(list)
+    original_df = pl.read_excel(dir_path / 'original_instagram_data.xlsx')
+    error_df = pl.read_csv(dir_path / 'error_out_test.csv')
+    postprocess_df = pl.read_csv(dir_path / 'n_images_video_with_id.csv')
+    merge_df = postprocess_df.join(error_df, on='id', how='outer')
+    merge_df1 = original_df.join(merge_df, on='id')
 
-label_df = df.pivot(index='id', columns='id_', values='notes')
-label_df = label_df.with_columns(pl.col('id').cast(pl.Int64))
+    def select_images(dir_path: PathLike) -> pl.DataFrame:
 
-final_df = label_df.join(merge_df, on='id',how='outer')
+        label_df = pl.read_csv(dir_path / 'test2.csv')
 
-final_df.write_excel('/Users/wei/Documents/cara_network/amr_igdata/final_dftest.xlsx')
+        post_id = label_df['filename'].to_list()
+        for ids in post_id:
+            ret['id'].append(ids.split('_')[1])
+        note = label_df['notes'].to_list()
+        for notes in note:
+            if 1 <= notes <= 8:
+                ret['selected_images'].append(True)
+            else:
+                ret['selected_images'].append(False)
+        label_df = pl.DataFrame(ret)
+        label_df = label_df.group_by('id').agg([pl.col('selected_images')])
+        label_df = label_df.with_columns(pl.col('id').cast(pl.Int64))
+        return label_df
+
+    final_df = select_images(dir_path)
+    print(final_df)
+    final_df = merge_df1.join(final_df, on='id', how='outer')
+    print(final_df)
+
+    if final_out is not None:
+        final_df.write_excel(final_out)
+
+
+if __name__ == '__main__':
+
+    dir_path = Path('/Users/wei/Documents/cara_network/amr_igdata/output')
+    final_out = Path('/Users/wei/Documents/cara_network/amr_igdata/output/final_test.xlsx')
+    merge_all(dir_path, final_out)
