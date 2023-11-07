227b5762e03371bd7cf0b7327d494c6583118aa5
Wei-Jan Chang <cc9868422@gmail.com>
2023-11-07 12:57:56 +0800

    bar

--- amr/src/data_visulisation.py
+++ amr/src/data_visulisation.py
@@ -7,10 +7,14 @@ data = pd.read_excel('/Users/wei/Documents/cara_network/amr_igdata/output/final_
 print(data.describe())
 print(data.columns)
 
-# Creating a Histogram
+# Histogram
 
 sns.histplot(data['cat'], kde=False).set_title('Histogram of categories')
-plt.show()
+# plt.show()
 
 sns.histplot(data['cat'], kde=True).set_title('Histogram of categories')  # kde: plot density estimation
+# plt.show()
+
+# Bar
+sns.barplot(data, x='year', y='likesCount', hue='cat')  # add a second layer of grouping with hue
 plt.show()
