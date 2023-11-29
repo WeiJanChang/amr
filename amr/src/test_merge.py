bce56484a3ad5f80eea5236b903fa92969b19206
Wei-Jan Chang <cc9868422@gmail.com>
2023-11-29 15:38:42 +0000

    move kappa codes

--- amr/src/test_merge.py
+++ amr/src/test_merge.py
@@ -5,7 +5,6 @@ merge df from Simon's application (images) and original instagram data (11 json 
 import collections
 from pathlib import Path
 from typing import Union
-import pandas as pd
 import polars as pl
 
 PathLike = Union[Path | str]
@@ -54,25 +53,7 @@ def merge_all(dir_path: PathLike, final_out: PathLike = None) -> pl.DataFrame:
     return final_df
 
 
-def cara_messages(df: pd.DataFrame):
-    from sklearn.metrics import cohen_kappa_score
-    df['cat_1_message'] = df['Cat 1'].map({v: k for k, v in messages.items()})
-    df['wei_message'] = df['Wei Cat 1'].map({v: k for k, v in messages.items()})
-    df['sana_message'] = df['Sana Cat 1'].map({v: k for k, v in messages.items()})
-    df['akke_message'] = df['Akke_cat1'].map({v: k for k, v in messages.items()})
-    df['patricia_message'] = df['Patricia_cat1'].map({v: k for k, v in messages.items()})
-    df.fillna(0, inplace=True)
-    # Calculate Cohen's Kappa
-    kappa_cat1_wei = cohen_kappa_score(df['cat_1_message'], df['wei_message'])
-    print(f"Cohen's Kappa between cat_1_message and wei_message: {kappa_cat1_wei}")
-
-    kappa_cat1_sana = cohen_kappa_score(df['cat_1_message'], df['sana_message'])
-    print(f"Cohen's Kappa between cat_1_message and sana_message: {kappa_cat1_sana}")
-
-
 if __name__ == '__main__':
     dir_path = Path('/Users/wei/Documents/cara_network/amr_igdata/output')
     final_out = Path('/Users/wei/Documents/cara_network/amr_igdata/output/final_test.parquet')
     merge_all(dir_path, final_out)
-    # df = pd.read_excel("/Users/wei/Documents/cara_network/amr_igdata/output/kappa_test_file_27Sep.xlsx")
-    # cara_messages(df)
