37b76dbe84722a2a2b250a4cb7729c0506b0f8ff
Wei-Jan Chang <cc9868422@gmail.com>
2023-11-29 16:45:26 +0000

    added merge_all

--- amr/src/images_download.py
+++ amr/src/images_download.py
@@ -21,7 +21,7 @@ def create_latestpost_info(directory: PathLike) -> LatestPostInfo:
     return ret.remove_unused_fields().remove_duplicate()
 
 
-def download_image(info: LatestPostInfo, output_path: PathLike, error_out: PathLike = None):
+def download_image(info: LatestPostInfo, output_path: PathLike, error_out: PathLike = None) -> pl.DataFrame:
     """
     convert LatestPostInfo to polars df. get all ids and urls from df.
     for each download using _download and save images and videos to output_path
@@ -64,8 +64,7 @@ def download_image(info: LatestPostInfo, output_path: PathLike, error_out: PathL
     return error_df
 
 
-def _download(loader: Instaloader, context: InstaloaderContext, post_id: str, post_url: str, output_path: PathLike,
-              exist_out: PathLike = None):
+def _download(loader: Instaloader, context: InstaloaderContext, post_id: str, post_url: str, output_path: PathLike):
     """
     Download images and videos via url and save name based on id from each post
     :param loader: Instaloader
@@ -137,14 +136,46 @@ def download_postprocess(output_path: PathLike, new_dir: PathLike,
     return df
 
 
-def merge_all(original_df: PathLike = Path,
-              post_pro_df3: PathLike = Path) -> pl.DataFrame:
-    # original_df =
-    # post_pro_df3 =
-    # merge_df = original_df.join(post_pro_df3, on='id')
+def merge_all(dir_path: PathLike, final_out: PathLike = None) -> pl.DataFrame:
+    messages = {1: 'Humour', 2: 'Shock/Disgust/Fear', 3: 'Educational/Informative',
+                4: 'Personal Stories', 5: 'Opportunistic', 6: 'Advocacy', 9: 'Non-selected'}
+    selected_keys = {key: value for key, value in messages.items() if key in [1, 2, 3, 4, 5, 6]}
+    ret = collections.defaultdict(list)
+    original_df = pl.read_excel(dir_path / 'original_instagram_data.xlsx')
+    error_df = pl.read_csv(dir_path / 'error_out_test.csv')
+    postprocess_df = pl.read_csv(dir_path / 'n_images_video_with_id.csv')
+    merge_df = postprocess_df.join(error_df, on='id', how='outer')
+    merge_df1 = original_df.join(merge_df, on='id')
 
-    # return merge_df
-    pass
+    def select_images(dir_path: PathLike, label_out: PathLike = None) -> pl.DataFrame:
+        label_df = pl.read_csv(dir_path / 'test2.csv')
+        post_id = label_df['filename'].to_list()
+        for ids in post_id:
+            ret['id'].append(ids.split('_')[1])
+        note = label_df['notes'].to_list()
+        for notes in note:
+            if notes in selected_keys:
+                ret['selected_images'].append(str(1))  # selected
+                ret['notes'].append(str(notes))
+            else:
+                ret['selected_images'].append(str(0))  # non-selected
+                ret['notes'].append(str(notes))
+        label_df = pl.DataFrame(ret)
+        label_df = label_df.group_by('id').agg([pl.col('selected_images', 'notes')])
+        label_df = label_df.with_columns(pl.col('id').cast(pl.Int64))
+
+        if label_out is not None:
+            label_df.write_excel(label_out)
+
+        return label_df
+
+    final_df = select_images(dir_path)
+    final_df = merge_df1.join(final_df, on='id', how='inner')
+
+    if final_out is not None:
+        final_df.write_excel(final_out)
+
+    return final_df
 
 
 if __name__ == '__main__':
@@ -156,3 +187,7 @@ if __name__ == '__main__':
     new_dir = Path('/Users/wei/Documents/cara_network/amr_igdata/instagram_images')
     save_path = '/Users/wei/Documents/cara_network/amr_igdata/output/n_images_video_with_id.csv'
     ret = download_postprocess(output_path, new_dir, out=save_path)
+
+    dir_path = Path('/Users/wei/Documents/cara_network/amr_igdata/output')
+    final_out = Path('/Users/wei/Documents/cara_network/amr_igdata/output/final_test.xlsx')
+    merge_all(dir_path, final_out)
